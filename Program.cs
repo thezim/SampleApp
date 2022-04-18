98b1c0dbc0b67c5d7e3d9d562c2962f55dd9a90d
Greg Zimmerman <thezim@gmail.com>
2022-04-18 12:06:52 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new System.Net.Mail.SmtpClient();
+        }
+    }
+}
